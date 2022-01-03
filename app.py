646d5cfff252da74fd6c5605a83119170df7ab67
huubinh49 <hocchui0409@gmail.com>
2022-01-03 21:31:54 +0700

    add naive bayes model

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 import streamlit as st
 import numpy as np
+from vncorenlp import VnCoreNLP
+import joblib
+import re
+vncorenlp_file = r'./VnCoreNLP/VnCoreNLP-1.1.1.jar'
+vncorenlp = VnCoreNLP(vncorenlp_file, annotators="wseg",  max_heap_size='-Xmx500m')
 
+stopwords = []
+with open("./vietnamese-stopwords.txt", 'r') as f:
+  stopwords = list(map(lambda x: x.replace('\n', ''), f.readlines()))
+
+def remove_stopwords(in_string):
+  return " ".join([x for x in in_string.split(" ") if x not in stopwords])
+
+def remove_punctuation(in_string):
+    in_string = in_string.replace('\n', ' ')
+    out_string = re.sub(r'[^\w\s]', '', in_string)
+
+    return out_string
+def tokenizer(in_string):
+    in_string = remove_stopwords(remove_punctuation(in_string.lower()))
+    return vncorenlp.tokenize(in_string)[0]
+
+trained_bayes = joblib.load("./TF-IDF_NaiveBayes.pkl")
 def naive_bayes(news):
-    return np.random.randint(0, 2)
+    # because this model return 0 for fake news and 1 for vice versa
+    return 1 - trained_bayes.predict([news])
 
 def logistic(news):
     return np.random.randint(0, 2)
 
 def predict(news, model):
     m = naive_bayes if model == 'Naive Bayes' else logistic
-    return f"Result: {m(news)}"
+    return f"Result: {'This is a fake news' if m(news)[0] == 1 else 'This is a fact news' }"
 
 st.title('Fake news detector')
 st.selectbox('Choose model', ('Naive Bayes', 'Logistic Regression'), key='selectbox_model')
