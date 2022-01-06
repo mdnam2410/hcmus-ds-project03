74b19052f27d0e74d3bc06bcf19485a5539e325c
Nam Mai <mdnam2410@gmail.com>
2022-01-06 09:53:18 +0700

    Squashed commit of the following:
    commit cc3785b12b951aa797215b1bcd743f2b4246a229
    Author: Duc Huy Le <huyle4470@gmail.com>
    Date:   Thu Jan 6 00:29:18 2022 +0700
    
        add svm.pkl
    
    commit 85b1785e02439b88e34ba1c6a8688335dcaeeda6
    Merge: 94733c6 5d1afe3
    Author: huubinh49 <55043994+huubinh49@users.noreply.github.com>
    Date:   Tue Jan 4 21:14:14 2022 +0700
    
        Merge pull request #1 from mdnam2410/Binh
    
        Binh
    
    commit 5d1afe3e3f6e4539d6809d825442e54dfc577f36
    Author: huubinh49 <55043994+huubinh49@users.noreply.github.com>
    Date:   Mon Jan 3 21:49:55 2022 +0700
    
        Delete .idea directory
    
    commit 793a444fbd41115da9641409b7a30af40e283cd5
    Author: huubinh49 <hocchui0409@gmail.com>
    Date:   Mon Jan 3 21:32:27 2022 +0700
    
        add naive bayes
    
    commit 646d5cfff252da74fd6c5605a83119170df7ab67
    Author: huubinh49 <hocchui0409@gmail.com>
    Date:   Mon Jan 3 21:31:54 2022 +0700
    
        add naive bayes model

--- app.py
+++ app.py
@@ -3,13 +3,40 @@ import numpy as np
 from vncorenlp import VnCoreNLP
 import joblib
 import re
+vncorenlp_file = r'./VnCoreNLP/VnCoreNLP-1.1.1.jar'
 
+# Call this function once
 @st.cache(hash_funcs={VnCoreNLP: id})
 def get_vncorenlp():
-    return VnCoreNLP(r'./VnCoreNLP/VnCoreNLP-1.1.1.jar', annotators='wseg', max_heap_size='-Xmx500m')
+    return VnCoreNLP(vncorenlp_file, annotators='wseg', max_heap_size='-Xmx500m')
 
+
+# ---- Naive Bayes model ----
+
+stopwords = []
+with open("./vietnamese-stopwords.txt", 'r') as f:
+  stopwords = list(map(lambda x: x.replace('\n', ''), f.readlines()))
+
+def remove_stopwords(in_string):
+  return " ".join([x for x in in_string.split(" ") if x not in stopwords])
+
+def remove_punctuation(in_string):
+    in_string = in_string.replace('\n', ' ')
+    out_string = re.sub(r'[^\w\s]', '', in_string)
+    return out_string
+
+vncorenlp = get_vncorenlp()
+def tokenizer(in_string):
+    in_string = remove_stopwords(remove_punctuation(in_string.lower()))
+    return vncorenlp.tokenize(in_string)[0]
+
+trained_bayes = joblib.load("./TF-IDF_NaiveBayes.pkl")
 def naive_bayes(news):
-    return np.random.randint(0, 2)
+    # because this model return 0 for fake news and 1 for vice versa
+    return 1 - trained_bayes.predict([news])
+
+
+# ---- Logistic regression model ----
 
 def logistic_preprocess(text: str):
     # Lowercase
@@ -36,19 +63,30 @@ def logistic_get_stopwords():
 def logistic_get_model():
     return joblib.load('logistic-regression-model/logistic_model.pkl')
 
-
-vncorenlp_tokenizer = get_vncorenlp()
+vncorenlp_tokenizer = vncorenlp
 logistic_stopwords = logistic_get_stopwords()
 logistic_model = logistic_get_model()
 def logistic(news):
-    return logistic_model.predict([news])[0]
+    return logistic_model.predict([news])
+
+
+# ---- SVM model ----
+
+svm_model = joblib.load('SVM/SVM.pkl')
+def svm(news):
+    return svm_model.predict([news])
 
 def predict(news, model):
-    m = naive_bayes if model == 'Naive Bayes' else logistic
-    return f"Result: {m(news)}"
+    if model == 'Naive Bayes':
+        m = naive_bayes
+    elif model == 'Logistic Regression':
+        m = logistic
+    else:
+        m = svm
+    return f"Result: {'This is a fake news' if m(news)[0] == 1 else 'This is a fact news' }"
 
 st.title('Fake news detector')
-st.selectbox('Choose model', ('Naive Bayes', 'Logistic Regression'), key='selectbox_model')
+st.selectbox('Choose model', ('Naive Bayes', 'Logistic Regression', 'SVM'), key='selectbox_model')
 st.text_input('Enter news to validate', key='textinput_news')
 if st.button('Predict', key='button_predict',):
     st.write(predict(st.session_state.textinput_news, st.session_state.selectbox_model))
